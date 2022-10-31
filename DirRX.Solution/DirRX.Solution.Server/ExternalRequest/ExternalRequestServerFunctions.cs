666cc1b87353d37434b077befc5383a353118595
Maksim Stetsuk <m.stetsuk@tanais.ru>
2022-10-31 23:43:11 +0300

    Справочник внешнии обращения [Rx]

---
+++ DirRX.Solution/DirRX.Solution.Server/ExternalRequest/ExternalRequestServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.Solution.ExternalRequest;
+
+namespace DirRX.Solution.Server
+{
+  partial class ExternalRequestFunctions
+  {
+
+  }
+}
